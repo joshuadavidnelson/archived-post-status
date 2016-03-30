4aee74e0f99e9fd419f4ab702e30807ac92c3127
Frankie Jarrett <fjarrett@gmail.com>
2016-03-29 22:18:53 -0500

    Update archived-post-status.php

--- archived-post-status.php
+++ archived-post-status.php
@@ -5,8 +5,11 @@
  * Version: 0.3.4
  * Author: Frankie Jarrett
  * Author URI: http://frankiejarrett.com
- * License: GPLv2+
  * Text Domain: archived-post-status
+ * 
+ * Copyright: © 2015 Frankie Jarrett.
+ * License: GNU General Public License v2.0
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  */
 
 /**
