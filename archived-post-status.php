d815c9c5a638e7fc33474573a46b6a7ae6460263
Frankie Jarrett <fjarrett@godaddy.com>
2016-04-13 12:55:01 -0500

    Code formatting

--- archived-post-status.php
+++ archived-post-status.php
@@ -6,27 +6,23 @@
  * Author: Frankie Jarrett
  * Author URI: http://frankiejarrett.com
  * Text Domain: archived-post-status
- *
- * Copyright: © 2015 Frankie Jarrett.
  * License: GNU General Public License v2.0
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * Copyright © 2016 Frankie Jarrett. All Rights Reserved.
  */
 
 /**
- * Define plugin constants
+ * Define plugin constants.
  */
 define( 'ARCHIVED_POST_STATUS_VERSION', '0.3.4' );
-
 define( 'ARCHIVED_POST_STATUS_PLUGIN', plugin_basename( __FILE__ ) );
-
 define( 'ARCHIVED_POST_STATUS_DIR', plugin_dir_path( __FILE__ ) );
-
 define( 'ARCHIVED_POST_STATUS_URL', plugin_dir_url( __FILE__ ) );
-
 define( 'ARCHIVED_POST_STATUS_LANG_PATH', dirname( ARCHIVED_POST_STATUS_PLUGIN ) . '/languages' );
 
 /**
- * Load languages
+ * Load languages.
  *
  * @action plugins_loaded
  */
@@ -35,26 +31,25 @@ function aps_i18n() {
 	load_plugin_textdomain( 'archived-post-status', false, ARCHIVED_POST_STATUS_LANG_PATH );
 
 }
-
 add_action( 'plugins_loaded', 'aps_i18n' );
 
 /**
- * Translations strings placeholder function
+ * Translations strings placeholder function.
  *
- * Translation strings that are not used elsewhere but Plugin Title and Description
- * are helt here to be picked up by Poedit. Keep these in sync with the actual plugin's
- * title and description.
+ * Translation strings that are not used elsewhere but
+ * Plugin Title and Description are helt here to be
+ * picked up by Poedit. Keep these in sync with the
+ * actual plugin's title and description.
  */
 function aps_i18n_strings() {
 
 	__( 'Archived Post Status', 'archived-post-status' );
-
 	__( 'Allows posts and pages to be archived so you can unpublish content without having to trash it.', 'archived-post-status' );
 
 }
 
 /**
- * Register a custom post status for Archived
+ * Register a custom post status for Archived.
  *
  * @action init
  */
@@ -73,11 +68,10 @@ function aps_register_archive_post_status() {
 	register_post_status( 'archive', $args );
 
 }
-
 add_action( 'init', 'aps_register_archive_post_status' );
 
 /**
- * Returns TRUE if on the frontend, otherwise FALSE
+ * Check if we are on the frontend.
  *
  * @filter aps_status_arg_exclude_from_search
  *
@@ -88,18 +82,17 @@ function aps_is_frontend() {
 	return ! is_admin();
 
 }
-
 add_filter( 'aps_status_arg_exclude_from_search', 'aps_is_frontend' );
 
 /**
- * Returns TRUE if current user can view, otherwise FALSE
+ * Check if the current user can view Archived content.
  *
  * @return bool
  */
 function aps_current_user_can_view() {
 
 	/**
-	 * Default capability to grant ability to view Archived content
+	 * Default capability to grant ability to view Archived content.
 	 *
 	 * @since 0.3.0
 	 *
@@ -112,14 +105,14 @@ function aps_current_user_can_view() {
 }
 
 /**
- * Whether editing archived posts is allowed
+ * Check if editing Archived content is allowed.
  *
  * @return bool
  */
 function aps_is_editing_allowed() {
 
 	/**
-	 * Archived posts are not editable by default
+	 * Archived content is not editable by default.
 	 *
 	 * @since 0.3.5
 	 *
@@ -130,10 +123,10 @@ function aps_is_editing_allowed() {
 }
 
 /**
- * Filter archived post titles on the frontend
+ * Filter Archived post titles on the frontend.
  *
- * @param string $title
- * @param int    $post_id (optional)
+ * @param  string $title
+ * @param  int    $post_id (optional)
  *
  * @return string
  */
@@ -159,16 +152,16 @@ function aps_the_title( $title, $post_id = null ) {
 add_filter( 'the_title', 'aps_the_title', 10, 2 );
 
 /**
- * Check if a post type should NOT be using the Archived status
+ * Check if a post type should NOT be using the Archived status.
  *
- * @param string $post_type
+ * @param  string $post_type
  *
  * @return bool
  */
 function aps_is_excluded_post_type( $post_type ) {
 
 	/**
-	 * Prevent the Archived status from being used on these post types
+	 * Prevent the Archived status from being used on these post types.
 	 *
 	 * @since 0.1.0
 	 *
@@ -176,18 +169,12 @@ function aps_is_excluded_post_type( $post_type ) {
 	 */
 	$excluded = (array) apply_filters( 'aps_excluded_post_types', array( 'attachment' ) );
 
-	if ( in_array( $post_type, $excluded ) ) {
-
-		return true;
-
-	}
-
-	return false;
+	return in_array( $post_type, $excluded );
 
 }
 
 /**
- * Modify the DOM on post screens
+ * Modify the DOM on post screens.
  *
  * @action admin_footer-post.php
  */
@@ -208,18 +195,17 @@ function aps_post_screen_js() {
 		jQuery( document ).ready( function( $ ) {
 			$( '#post_status' ).append( '<option value="archive"><?php esc_html_e( 'Archived', 'archived-post-status' ) ?></option>' );
 			$( '#post-status-display' ).text( '<?php esc_html_e( 'Archived', 'archived-post-status' ) ?>' );
-		});
+		} );
 		</script>
 		<?php
 
 	}
 
 }
-
 add_action( 'admin_footer-post.php', 'aps_post_screen_js' );
 
 /**
- * Modify the DOM on edit screens
+ * Modify the DOM on edit screens.
  *
  * @action admin_footer-edit.php
  */
@@ -241,7 +227,7 @@ function aps_edit_screen_js() {
 
 		$.each( $rows, function() {
 			disallowEditing( $( this ) );
-		});
+		} );
 	<?php endif; ?>
 
 		$( 'select[name="_status"]' ).append( '<option value="archive"><?php esc_html_e( 'Archived', 'archived-post-status' ) ?></option>' );
@@ -252,7 +238,7 @@ function aps_edit_screen_js() {
 			    is_archived = $row.hasClass( 'status-archive' );
 
 			$option.prop( 'selected', is_archived );
-		});
+		} );
 
 	<?php if ( ! aps_is_editing_allowed() ) : ?>
 		$( '.inline-edit-row' ).on( 'remove', function() {
@@ -262,7 +248,7 @@ function aps_edit_screen_js() {
 			if ( $row.hasClass( 'status-archive' ) ) {
 				disallowEditing( $row );
 			}
-		});
+		} );
 
 		function disallowEditing( $row ) {
 			var title = $row.find( '.column-title a.row-title' ).text();
@@ -271,16 +257,15 @@ function aps_edit_screen_js() {
 			$row.find( '.row-actions .edit' ).remove();
 		}
 	<?php endif; ?>
-	});
+	} );
 	</script>
 	<?php
 
 }
-
 add_action( 'admin_footer-edit.php', 'aps_edit_screen_js' );
 
 /**
- * Prevent archived content from being edited
+ * Prevent Archived content from being edited.
  *
  * @action load-post.php
  */
@@ -293,8 +278,7 @@ function aps_load_post_screen() {
 	}
 
 	$post_id = (int) filter_input( INPUT_GET, 'post', FILTER_SANITIZE_NUMBER_INT );
-
-	$post = get_post( $post_id );
+	$post    = get_post( $post_id );
 
 	if (
 		is_null( $post )
@@ -308,8 +292,7 @@ function aps_load_post_screen() {
 
 	}
 
-	$action = filter_input( INPUT_GET, 'action', FILTER_SANITIZE_STRING );
-
+	$action  = filter_input( INPUT_GET, 'action', FILTER_SANITIZE_STRING );
 	$message = (int) filter_input( INPUT_GET, 'message', FILTER_SANITIZE_NUMBER_INT );
 
 	// Redirect to list table after saving as Archived
@@ -317,10 +300,9 @@ function aps_load_post_screen() {
 
 		wp_safe_redirect(
 			add_query_arg(
-				array(
-					'post_type' => $post->post_type,
-				),
-				admin_url( 'edit.php' )
+				'post_type',
+				$post->post_type,
+				self_admin_url( 'edit.php' )
 			),
 			302
 		);
@@ -335,16 +317,15 @@ function aps_load_post_screen() {
 	);
 
 }
-
 add_action( 'load-post.php', 'aps_load_post_screen' );
 
 /**
- * Display custom post state text next to post titles that are Archived
+ * Display custom post state text next to post titles that are Archived.
  *
  * @filter display_post_states
  *
- * @param array  $post_states  An array of post display states
- * @param object $post         WP_Post
+ * @param  array   $post_states
+ * @param  WP_Post $post
  *
  * @return array
  */
@@ -370,17 +351,16 @@ function aps_display_post_states( $post_states, $post ) {
 	);
 
 }
-
 add_filter( 'display_post_states', 'aps_display_post_states', 10, 2 );
 
 /**
- * Close comments and pings when content is archived
+ * Close comments and pings when content is Archived.
  *
  * @action save_post
  *
- * @param int    $post_id  Post ID
- * @param object $post     WP_Post
- * @param bool   $update   Whether this is an existing post being updated or not
+ * @param int     $post_id
+ * @param WP_Post $post
+ * @param bool    $update
  */
 function aps_save_post( $post_id, $post, $update ) {
 
@@ -413,5 +393,4 @@ function aps_save_post( $post_id, $post, $update ) {
 	}
 
 }
-
 add_action( 'save_post', 'aps_save_post', 10, 3 );
