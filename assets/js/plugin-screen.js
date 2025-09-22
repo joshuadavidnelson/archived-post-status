20c068a392c449b4a56618384136b6f639ba3b45
Joshua Nelson <josh@joshuadnelson.com>
2025-09-21 22:16:17 -0700

    add warning pop up on deactivation with archived content

---
+++ assets/js/plugin-screen.js
@@ -0,0 +1,15 @@
+document.addEventListener( 'DOMContentLoaded', function() {
+
+    var deactivateLink = document.querySelector( 'tr[data-slug="archived-post-status"] .deactivate a') ;
+
+    if ( deactivateLink ) {
+        deactivateLink.addEventListener( 'click', function(e) {
+            // translators: warning about deactivating the plugin with archived content.
+            var message = wp.i18n.__( 'Warning! Deactivating this plugin will remove the \'archive\' status and all content in that status will be hidden. To keep that content visible, unarchive it first and then deactivate this plugin.', 'archived-post-status' );
+
+            if ( archivedPostStatus.hasArchivedPosts && ! confirm( message ) ) {
+                e.preventDefault();
+            }
+        });
+    }
+});
