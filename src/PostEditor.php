2e40693caa922f25d03cf2311ee6ca63f507d8f4
Joshua Nelson <josh@joshuadnelson.com>
2025-09-21 23:16:35 -0700

    move load-posts action into PostEditor class

--- src/PostEditor.php
+++ src/PostEditor.php
@@ -39,6 +39,9 @@ class PostEditor extends Feature {
 
 		// Add archive button to the classic editor.
 		add_action( 'post_submitbox_start', array( $this, 'post_submitbox_archive_button' ) );
+
+		// Prevent Archived content from being edited.
+		add_action( 'load-post.php', array( $this, 'load_post_screen' ) );
 	}
 
 	/**
@@ -124,5 +127,50 @@ class PostEditor extends Feature {
 
 		// Otherwise, check if classic editor plugin is active
 		return function_exists( 'is_plugin_active' ) && is_plugin_active( 'classic-editor/classic-editor.php' );
+	}
+
+	/**
+	 * Prevent Archived content from being edited.
+	 *
+	 * @action load-post.php
+	 */
+	public function load_post_screen() {
+
+		if ( ! aps_is_read_only() ) {
+			return;
+		}
+
+		$post_id = absint( get_query_var( 'post' ) );
+		$post    = get_post( $post_id );
+
+		if ( is_null( $post )
+			|| ! aps_is_supported_post_type( $post->post_type )
+			|| 'archive' !== $post->post_status ) {
+				return;
+		}
+
+		$action  = esc_attr( get_query_var( 'action' ) );
+		$message = absint( get_query_var( 'message' ) );
+
+		// Redirect to list table after saving as Archived.
+		if ( 'edit' === $action && 1 === $message ) {
+
+			wp_safe_redirect(
+				add_query_arg(
+					'post_type',
+					$post->post_type,
+					self_admin_url( 'edit.php' )
+				),
+				302
+			);
+
+			exit;
+		}
+
+		// translators: Error message when trying to edit an Archived post.
+		wp_die(
+			__( "You can't edit this item because it has been Archived. Please change the post status and try again.", 'archived-post-status' ),
+			__( 'WordPress &rsaquo; Error' ) // phpcs:ignore
+		);
 	}
 }
