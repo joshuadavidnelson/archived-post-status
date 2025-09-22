bc4b207b2b8daebd44bedc9dad442a7a14d181a8
Joshua Nelson <josh@joshuadnelson.com>
2025-09-21 22:30:35 -0700

    Update plugin-screen.js

--- assets/js/plugin-screen.js
+++ assets/js/plugin-screen.js
@@ -1,9 +1,9 @@
 document.addEventListener( 'DOMContentLoaded', function() {
 
-    var deactivateLink = document.querySelector( 'tr[data-slug="archived-post-status"] .deactivate a') ;
+    const deactivateLink = document.querySelector( 'tr[data-slug="archived-post-status"] .deactivate a');
 
     if ( deactivateLink ) {
-        deactivateLink.addEventListener( 'click', function(e) {
+        deactivateLink.addEventListener( 'click', (e) => {
             // translators: warning about deactivating the plugin with archived content.
             var message = wp.i18n.__( 'Warning! Deactivating this plugin will remove the \'archive\' status and all content in that status will be hidden. To keep that content visible, unarchive it first and then deactivate this plugin.', 'archived-post-status' );
 
