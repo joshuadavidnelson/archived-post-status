05b17e2f59865a7fc2d79e40d915db8cedddef8d
Joshua Nelson <josh@joshuadnelson.com>
2026-02-16 15:48:17 -0800

    Update bootstrap.php

--- tests/php/bootstrap.php
+++ tests/php/bootstrap.php
@@ -10,10 +10,10 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 WP_Mock::setUsePatchwork( true );
 WP_Mock::bootstrap();
 
-define( 'PLUGIN_PATH', dirname( __DIR__, 2 ) );
+define( 'APS_PLUGIN_PATH', dirname( __DIR__, 2 ) );
 
 require_once __DIR__ . '/includes/common.php';
 require_once __DIR__ . '/includes/TestCase.php';
 
 // Load plugin files.
-require_once PLUGIN_PATH . '/src/archived-post-status.php';
+require_once APS_PLUGIN_PATH . '/src/archived-post-status.php';
